1d1ad8c73375614e3529a1fc94c54cbd781bc145
Stephen Beckett <beckett.stephenj@gmail.com>
2024-04-11 13:33:04 -0400

    Update create_lookuptable.R

--- R/create_lookuptable.R
+++ R/create_lookuptable.R
@@ -1,26 +1,25 @@
 
-#reference individual level quotas (from Paul) in terms of [μmol N/indiv]
-referenceQ = c(1.7*10^-7.0, 6.1*10^-10.0 , 10^-11.0, 10^-11.0)
-#convert from micromole to milimole [ μmol -->  mmol]
-referenceQ = referenceQ*1000
-referenceName = c("diatom cells", "Pro. cells", "diatom viruses", "Pro. viruses")
 
-#some reference values in L/day to encompass organism densities and contact rates
-refvalsLperday = rev(10^c(-15:9))
+referenceQ = c(1.7*10^-7.0, 6.1*10^-10.0 , 10^-11.0, 10^-11.0)*1000
+referenceName = c("diatom biomass", "Pro.  biomass", "diatom virus  biomass", "Pro. virus  biomass")
+
+#some reference values in indiv/L
+refvals_perL = rev(10^c(0:9))
 
 #create table
-mmolcubicm_MAT = matrix(NA,length(refvalsLperday),length(referenceQ))
+mmolcubicm_MAT = matrix(NA,length(refvals_perL),length(referenceQ))
 
-for(aa in 1:length(refvalsLperday)){
+for(aa in 1:length(refvals_perL)){
 	for(bb in 1:length(referenceQ)){
-		mmolcubicm_MAT[aa,bb] = IndivPerL_2_BiomassmmolPerCubicmetre(refvalsLperday[aa],referenceQ[bb])
+		mmolcubicm_MAT[aa,bb] = IndivPerL_2_BiomassmmolPerCubicmetre(refvals_perL[aa],referenceQ[bb])
 	}
 }
 
 #put into tabular format
-JJ = cbind(refvalsLperday,mmolcubicm_MAT)
+JJ = cbind(refvals_perL,mmolcubicm_MAT)
 JJ = as.data.frame(JJ)
-names(JJ) = c("reference [ L/day ]", paste("[mmol N m^-3]",referenceName))
+names(JJ) = c("reference [ indiv/L ]", paste("[mmol N m^-3]",referenceName))
+
 
 #write to file
-write.csv(JJ,file = "lookuptable_Lday_mmolcubicm.csv")
+write.csv(JJ,file = "lookuptable_indivperL_mmolNpercubicm.csv")
